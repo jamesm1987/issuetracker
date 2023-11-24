7081283600bfeec39e67dc51aada074f26418ec0
James McCormack <james@wirebox.co.uk>
2023-11-24 12:03:23 +0000

    style create issue

--- resources/views/projects/issues/create.blade.php
+++ resources/views/projects/issues/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Create a new issue.
             </h2>
@@ -8,7 +8,7 @@
     </x-slot>
 
     <div class="pt-1 pb-12 bg-white">
-        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-20">
 
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-6">
                 <div class="p-6 text-gray-900">
